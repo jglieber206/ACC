99f9dcba452db6a0f4e96441dc81cd680a4bf41d
Jace Lieberman <jglieber@gmail.com>
2015-08-19 13:47:13 -0700

    Restructure endpoint APIs for database integration pre-database code commit WIP ACC-2

--- accapp.rb
+++ accapp.rb
@@ -3,87 +3,160 @@ require 'sinatra'
 require 'json'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
-@@last_project = 3
-@@projects = { 1 => {
-    "id"=> 1,
-    "title"=> "Jace's Project",
-    "attributes"=> [
-      { "name"=> "Fast", "id"=> 1001 },
-      { "name"=> "Accurate", "id"=> 1002 },
-      { "name"=> "Secure", "id"=> 1003 },
-      { "name"=> "Predictable", "id"=> 1004 }
-    ],
-    "components"=> [
-      { "name"=> "dokidoki", "id"=> 2001 },
-      { "name"=> "marketing", "id"=> 2002 },
-      { "name"=> "love index", "id"=> 2003 }
-    ]
+
+# variable declarations
+
+@@projects = {
+  1 => {
+    "id" => 1,
+    "title" => "Jace's Project"
   },
   2 => {
-    "id"=> 2,
-    "title"=> "Drew's Project",
-    "attributes"=> [
-      { "name"=> "Fast", "id"=> 1001 },
-      { "name"=> "Accurate", "id"=> 1002 },
-      { "name"=> "Usable", "id"=> 1005 },
-      { "name"=> "Stable", "id"=> 1006 }
-    ],
-    "components"=> [
-      { "name"=> "dokidoki", "id"=> 2001 },
-      { "name"=> "blog", "id"=> 2004 },
-      { "name"=> "admin", "id"=> 2005 }
-    ]
+    "id" => 2,
+    "title" => "Drew's Project"
   },
   3 => {
-    "id"=> 3,
-    "title"=> "Joel's Project",
-    "attributes"=> [
-      { "name"=> "Reliable", "id"=> 1007 },
-      { "name"=> "Functional", "id"=> 1008 },
-      { "name"=> "Fast", "id"=> 1001 },
-      { "name"=> "Stable", "id"=> 1006 }
-    ],
-    "components"=> [
-      { "name"=> "dokidoki", "id"=> 2001 },
-      { "name"=> "admin", "id"=> 2005 },
-      { "name"=> "love index", "id"=> 2003 }
-    ]
-  }}
-  @@template = {
-    "title"=> "New Project",
-    "attributes"=> [
-      {"name"=> "", "id"=> ""}
-    ],
-    "components"=> [
-      {"name"=> "", "id"=> ""}
-    ]
+    "id" => 3,
+    "title" => "Joel's Project"
   }
+}
 
-get '/projects/:id' do
-  @@projects[params['id'].to_i].to_json
-end
+@@attributes = {
+  1 => {
+    "id" => 1,
+    "name" => "Fast",
+    "project_id" => 1
+  },
+  2 => {
+    "id" => 2,
+    "name" => "Accurate",
+    "project_id" => 1
+  },
+  3 => {
+    "id" => 3,
+    "name" => "Secure",
+    "project_id" => 2
+  },
+  4 => {
+    "id" => 4,
+    "name" => "Predictable",
+    "project_id" => 2
+  },
+   5 => {
+    "id" => 5,
+    "name" => "Fun",
+    "project_id" => 3
+  },
+  6 => {
+    "id" => 6,
+    "name" => "Simple",
+    "project_id" => 3
+  }
+}
+@@components = {
+  1 => {
+    "id" => 1,
+    "name" => "dokidoki",
+    "project_id" => 1
+  },
+  2 => {
+    "id" => 2,
+    "name" => "marketing",
+    "project_id" => 1
+  },
+  3 => {
+    "id" => 3,
+    "name" => "blog",
+    "project_id" => 2
+  },
+  4 => {
+    "id" => 4,
+    "name" => "admin",
+    "project_id" => 2
+  },
+  5 => {
+    "id" => 5,
+    "name" => "Predictable",
+    "project_id" => 3
+  },
+  6 => {
+    "id" => 6,
+    "name" => "love index",
+    "project_id" => 3
+  }
+}
+  @@last_project = @@projects.length
+  @@last_attribute = @@attributes.length
+  @@last_component = @@components.length
 
+###############################################
+## Project list & general endpoint functions ##
+###############################################
+
+## Get all projects
 get '/projects' do
   @@projects.to_json
 end
 
+## Get project by id
+get '/projects/:id' do
+
+  @@projects[params['id'].to_i].to_json
+end
+
+## Add new project
 post '/projects' do
-  # request.body.rewind
   payload = JSON.parse(request.body.read)
   @@last_project += 1
   payload['id'] = @@last_project
   @@projects[@@last_project] = payload
 end
 
+## Delete specific project (including all properties of project)
+delete '/projects/:id' do
+  @@projects.delete params['id'].to_i
+end
+
+## Route to app functionality
 get '/' do
   File.read('public/index.html')
 end
 
+## Route to any project file (must specify)
 get '/public/:filename' do
   File.read("public/#{params['filename']}")
 end
 
-delete '/projects/:id' do
-  @@projects.delete params['id'].to_i
-  return 200 # or new list of projects as json
+#########################################
+## Functions within a specific project ##
+#########################################
+
+## Access attributes list for a specified project
+get '/projects/:id/attributes' do
+  currentProject = @@projects[params['id'].to_i]
+  attributeList = {}
+  counter = 0
+  @@attributes.each do |attribute, value|
+    if value['project_id'] == currentProject['id']
+      currentAttribute = { counter => value }
+      attributeList.merge!(currentAttribute)
+      counter += 1
+    end
+  end
+  return attributeList
+end
+
+## Access components list for a specified project
+get '/projects/:id/components' do
+  currentProject = @@projects[params['id'].to_i]
+  componentList = {}
+  counter = 0
+  @@components.each do |component, value|
+    if value['project_id'] == currentProject['id']
+      currentComponent = { counter => value }
+      componentList.merge!(currentComponent)
+      counter += 1
+    end
+  end
+  return componentList
 end
