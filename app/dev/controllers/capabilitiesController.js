dc191ddfdcc5c7b0a99e98a1a593f20a01d69933
Jake Barnett <mrjbarnett@gmail.com>
2015-09-28 16:40:21 -0700

    Add capability names to grid intersection

--- app/dev/controllers/capabilitiesController.js
+++ app/dev/controllers/capabilitiesController.js
@@ -3,13 +3,10 @@ var app = require('../app.js')
 
 app.controller('CapabilitiesController', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
-    var my_attr = "";
-    var my_comp = "";
-
     $scope.addCapability = function(proj_id) {
       $http({
         method: 'POST',
-        url: '/attributes/'+my_attr+'/components/'+my_comp,
+        url: '/attributes/'+$scope.currentAttribute.id+'/components/'+$scope.currentComponent.id,
         data: { name: this.newCapName, project_id: proj_id, code: this.newCapCode, url: this.newCapUrl, oauth: this.newCapOauth }
       }).success(function() { console.log("added capability!") })
       .error(function() { console.log("error adding capability") });
@@ -36,38 +33,41 @@ app.controller('CapabilitiesController', ['$scope', '$http', '$rootScope', funct
       .error(function() { console.log("error deleting capability") })
     }
 
-    $scope.getCellCapabilities = function(attr_id, comp_id) {
-      var capsInCell = [];
-      for (var i = 0; i < $scope.projectMaps.length; i++) {
-        if ($scope.projectMaps[i].attribute_id == attr_id && $scope.projectMaps[i].component_id == comp_id) {
-          cap = getIndexById($scope.projectMaps[i].capability_id, $scope.projectCapabilities);
-          capsInCell.push($scope.projectCapabilities[cap]);
-        }
-      }
-      $scope.colorCell(capsInCell);
-      return capsInCell;
-    }
 
-    $scope.setAttrAndComp = function(attr_id, comp_id) {
-      console.log("****************: " + attr_id + "*********" + comp_id)
-      my_attr = attr_id;
-      my_comp = comp_id;
-      console.log(my_attr);
-      console.log(my_comp);
-    }
+    $scope.currentAttribute = null;
+    $scope.currentComponent = null;
 
-
-    // $scope.currentAttribute = null;
     $scope.getCurrentAttr = function(attr_id) {
-      attrIndex = getIndexById(attr_id, $scope.attributesList)
-      $scope.currentAttribute = $scope.attributesList[attrIndex]
+      attrIndex = getIndexById(attr_id, $scope.attributesList);
+      $scope.currentAttribute = $scope.attributesList[attrIndex];
     }
-    // $scope.currentComponent = null;
+
+    $scope.getCurrentComp = function(comp_id) {
+      compIndex = getIndexById(comp_id, $scope.attributesList);
+      $scope.currentComponent = $scope.componentsList[compIndex];
+    }
+
 
     $scope.capsInCell = [];
     $scope.cellCapList = function(capabilities) {
       $scope.capsInCell = capabilities;
     }
+
+    $scope.a = [];
+    $scope.getCellCapabilities = function(attr_id, comp_id) {
+      var a = [];
+      for (var i = 0; i < $scope.projectMaps.length; i++) {
+        if ($scope.projectMaps[i].attribute_id === attr_id && $scope.projectMaps[i].component_id === comp_id) {
+          cap = getIndexById($scope.projectMaps[i].capability_id, $scope.projectCapabilities);
+          a.push($scope.projectCapabilities[cap]);
+          console.log($scope.projectCapabilities[cap])
+        }
+      }
+      $scope.colorCell(a);
+      $scope.a = a;
+      return a;
+    }
+
     $scope.colorCell = function(caps) {
       var result = true;
       for (var i = 0; i < caps.length; i++) {
