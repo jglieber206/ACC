cdf33f5c533cad65521d368282bf2cca18f44276
Jake Barnett <jake@apptentive.com>
2015-11-12 11:36:47 -0800

    cleanup enviroment config

--- config/environments.rb
+++ config/environments.rb
@@ -1,14 +1,14 @@
 #The environment variable DATABASE_URL should be in the following format:
 # => postgres://{user}:{password}@{host}:{port}/path
 configure :production, :development do
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/development')
+	# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/development')
 
 	ActiveRecord::Base.establish_connection(
-			:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-			:host     => db.host,
-			:username => db.user,
-			:password => db.password,
-			:database => db.path[1..-1],
-			:encoding => 'utf8'
+			:adapter => ENV['DB_ADAPTER'],
+			:database => ENV['DB_DATABASE'],
+			:port => ENV['DB_PORT'],
+			:username => ENV['DB_USERNAME'],
+			:password => ENV['DB_PASSWORD'],
+			:host     => ENV['DB_HOST'],
 	)
 end
